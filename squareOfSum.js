e441f14813966ea06399fdd2c2cd89dfe442d988
Grader <labcontext@gmail.com>
2017-01-15 20:06:18 +0500

    UP 2

--- squareOfSum.js
+++ squareOfSum.js
@@ -2,8 +2,8 @@
 // суммы двух чисел по формуле: a^2 + 2ab + b^2
 // BEGIN (write your solution here)
 const squareOfSum = (a, b) => {
-  const qua = Math.pow(a);
-  const qub = Math.pow(b);
+  const qua = Math.pow(a, 2);
+  const qub = Math.pow(b, 2);
   return (qua + 2*a*b + qub);
 };
 // END
