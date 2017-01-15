ca7296cc0efff8e2f2debea9cdf5b09ee847618c
Grader <labcontext@gmail.com>
2017-01-15 16:50:42 +0500

    init

---
+++ squareOfSum.js
@@ -0,0 +1,17 @@
+//Реализуйте функцию squareOfSum, которая находит квадрат
+// суммы двух чисел по формуле: a^2 + 2ab + b^2
+// BEGIN (write your solution here)
+const squareOfSum = (a, b) => {
+  const qua = Math.pow(a);
+  const qub = Math.pow(b);
+  return (qua + 2*a*b + qub);
+};
+// END
+//Variant 2
+// BEGIN (write your solution here)
+const squareOfSum = (a, b) => {
+  const qua = (a * a);
+  const qub = (b * b);
+  return (qua + 2*a*b + qub);
+};
+// END
