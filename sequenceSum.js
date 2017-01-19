4bdf380c5009ed2ff545876be54956108e84087b
Grader <labcontext@gmail.com>
2017-01-19 22:49:51 +0500

    sequenceSum

---
+++ sequenceSum.js
@@ -0,0 +1,9 @@
+//Рекурсия
+export const sequenceSum = (begin, end, step) => {
+  // BEGIN (write your solution here)
+  if (begin > end) {
+    return 0;
+  }
+  return begin + sequenceSum(begin + step, end, step);
+  // END
+};
